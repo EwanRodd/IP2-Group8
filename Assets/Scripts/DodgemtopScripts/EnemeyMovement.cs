ae14e1ca993e9fc2e6f5013be5dfcad9292ea689
Nemshi <mccarthyjack245@gmail.com>
2026-03-11 14:35:43 +0000

    Continued on DodgemTop

---
+++ Assets/Scripts/DodgemtopScripts/EnemeyMovement.cs
@@ -0,0 +1,27 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class EnemeyMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float vertPosition;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, transform.position.y - vertPosition, transform.position.z);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Boundary")
+        {
+           // transform.position = new Vector3(transform.position.x, transform.position.y - vertPosition, transform.position.z);
+            moveSpeed *= -1;
+        }
+    }
+}
