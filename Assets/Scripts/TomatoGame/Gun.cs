310f87481f52c5189b6d8dbcd5c4ae79d6b2708a
v0id456 <github.dodge044@passinbox.com>
2026-03-26 13:20:06 +0000

    Implementing controller input
    Integrating InputActions map by replacing legacy code with a cleaner version. Firing is now called by an event as opposed to checking every frame

--- Assets/Scripts/TomatoGame/Gun.cs
+++ Assets/Scripts/TomatoGame/Gun.cs
@@ -1,16 +1,32 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 
 public class Gun : MonoBehaviour
 {
+    private PlayerInput playerInput;
+
     public float range = 100f;
 
+    void Awake()
+    {
+        
+        playerInput = new PlayerInput();
 
+        // --New Updated input system to link with controller--
+        playerInput.Player.Fire.performed += OnFire;
+        
+    }
+
+
+    // !! Only needed for testing with mouse !!
     void Update()
     {
-        // For testing change to controller 
-        Pc_LeftMouseButtonDown();
+        // ------For testing --------
+        //Pc_LeftMouseButtonDown();
+
     }
 
     void Fire()
@@ -25,12 +41,29 @@ public class Gun : MonoBehaviour
             Debug.Log("HITTT");
         }
     }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+    }
+
+    private void OnFire(InputAction.CallbackContext context)
+    {
+        Fire();
+        Debug.Log("Fired using Input Actions");
+    }
     void Controller_OnFireButtonDown()
     {
         Fire();
         Debug.Log("right trigger pressed");
         
     }
+
     void Pc_LeftMouseButtonDown()
     {
         if (Input.GetMouseButtonDown(0))
